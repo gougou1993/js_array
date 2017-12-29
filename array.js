b15cf1ad1b9a0f79fdc6bf0bfa7e6c667fa12053
gougou1993 <1478168700@qq.com>
2017-12-29 21:05:17 +0800

    Update array.js

--- array.js
+++ array.js
@@ -4,3 +4,9 @@ var newArray = new Array();
 var newArray2 = new Array(12);
 # 数组的长度
 var length = array.length;
+# 给数组追加添加元素
+array.push(1);
+# 获得数组一个元素
+var popItem = array.pop();
+# for in遍历数组
+for(index in array){console.log(array[index]);};
